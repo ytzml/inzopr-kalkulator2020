d406172a1862df36445c1f5ed925ce1708acfd50
jacek <j@m.pl>
2020-04-05 15:19:14 +0200

    Funkcja get_info gotowa

--- kalkulator.py
+++ kalkulator.py
@@ -1,7 +1,12 @@
+def get_info():
+    print("Witaj, to jest prosty kalkulator!")
+
 def dodaj():
     print('Wprowadź dwie liczby a zostaną one dodane.')
     a = int(intput())
     b = int(input())
     print(a + b)
 
+    
+get_info()
 dodaj()
